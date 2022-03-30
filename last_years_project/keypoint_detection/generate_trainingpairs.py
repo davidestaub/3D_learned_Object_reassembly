45ce1cd706ef31ad334a289376b622fd45fd64a2
Julia Bazinska <julia@bazinski.com>
2022-03-30 16:48:35 +0200

    Compute fragment matchings in a heuristic way.

--- last_years_project/keypoint_detection/generate_trainingpairs.py
+++ last_years_project/keypoint_detection/generate_trainingpairs.py
@@ -1,48 +1,47 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from scipy.spatial.distance import cdist
 import time
 
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import cdist
+
 # standard deviation of random displacement
-sigma=0.001
+sigma = 0.001
 # folder, where the pointcloud data is
-data_folder='./data'
+data_folder = './data'
 # folder, where to save the training data
-out_folder='./training_data'
+out_folder = './training_data'
 
 n = 0
 m = 0
 p = 0
 
-names=[]
+names = []
 # uncomment for whole trainset
 # for i in range(1,6):
 #     if i == 2 or i==3: continue
 #     names.append(f'cat_seed_{i}')
 # for i in range (4,15):
 #     names.append(f'cube_20_seed_{i}')
-# for i in range (2,10):
-#     names.append(f'cylinder_20_seed_{i}')
+for i in range(2, 10):
+    names.append(f'cylinder_20_seed_{i}')
 # uncomment for whole testset
 # names=['cat_seed_0', 'cube_20', 'cube_20_seed_1', 'cube_20_seed_2', 'cube_20_seed_3', 'cylinder_20_seed_1']
-names=['cube_20_seed_4']
+# names=['cube_20_seed_4']
 
-names1_1v1=[]
-names2_1v1=[]
-names1_1vN=[]
-names2_1vN=[]
-names_FN=[]
-rng=np.random.default_rng()
+names1_1v1 = []
+names2_1v1 = []
+names1_1vN = []
+names2_1vN = []
+names_FN = []
+rng = np.random.default_rng()
 
-start=time.time()
+start = time.time()
 
 for name in names:
     print(f'\nProcessing {name}...')
-    fragment=[]
-    keypoints=[]
-    
+    fragment = []
+    keypoints = []
+
     # Load keypoints and pointcloud of each fragment
     for part in range(1000):
         try:
@@ -50,13 +49,13 @@ for name in names:
         except:
             print(f'{part} parts loaded.\n')
             break
-        
+
         # normalizing  roughly to [-1,1]
-        if name[0:3]=='cat':
-            fragment[part][:,:3]/=450
+        if name[0:3] == 'cat':
+            fragment[part][:, :3] /= 450
 
         print(fragment[part].shape)
-        if part==999:
+        if part == 999:
             print('WARNING: part limit reached, only loaded the first 1000 parts.')
             part += 1
 
@@ -66,28 +65,28 @@ for name in names:
     # for i in range(part):
     #     ax.scatter(fragment[i][:,0], fragment[i][:,2], fragment[i][:,1], color=cm.Set1(i/part) , s=1)#, fragment[:,3], fragment[:,4], fragment[:,5])
     # plt.show()
-    
+
     # save fragments with connected surfaces 1vN
     print('\nSaving connected 1vN\n')
     for i in range(part):
-        names2_temp=[]
-        fragment_set=np.zeros((1,6))
+        names2_temp = []
+        fragment_set = np.zeros((1, 6))
         for j in range(part):
             if i == j: continue
-            
+
             # search for corresponding points in two parts (distance below a treshold)
-            matches = np.count_nonzero(cdist(fragment[i][:,:3],fragment[j][:,:3])<1e-3)
+            matches = np.count_nonzero(cdist(fragment[i][:, :3], fragment[j][:, :3]) < 1e-3)
             print(f'Fragments {i} and {j} have {matches} matches')
 
             # if there are more than 100 matches, the parts are considered neighbours
-            if  matches > 100:
+            if matches > 100:
                 print('Its a match!')
                 names2_temp.append(f'{name}/{name}_{j}.npy')
                 # all matching parts are concatenated together in one file, as if they where one point cloud
-                fragment_set=np.concatenate((fragment_set, fragment[j]),axis=0)
+                fragment_set = np.concatenate((fragment_set, fragment[j]), axis=0)
 
         # delete row of zeros at the beginning
-        fragment_set=np.delete(fragment_set, 0, axis=0)
+        fragment_set = np.delete(fragment_set, 0, axis=0)
 
         # only generate a trainingpair, if neighbouring part was found
         if fragment_set.shape[0] != 0:
@@ -96,31 +95,31 @@ for name in names:
             names2_1vN.append(names2_temp)
 
             # add random displacement of points with a gaussian profile
-            noise_i=rng.normal(0,sigma,fragment[i].shape)
-            noise_set=rng.normal(0,sigma,fragment_set.shape)
+            noise_i = rng.normal(0, sigma, fragment[i].shape)
+            noise_set = rng.normal(0, sigma, fragment_set.shape)
 
             # save the two parts of the pairs as seperate .npy files
-            np.save(f'{out_folder}/connected_1vN/fragments_1/{m}.npy', fragment[i]+noise_i)
-            np.save(f'{out_folder}/connected_1vN/fragments_2/{m}.npy', fragment_set+noise_set)
+            np.save(f'{out_folder}/connected_1vN/fragments_1/{m}.npy', fragment[i] + noise_i)
+            np.save(f'{out_folder}/connected_1vN/fragments_2/{m}.npy', fragment_set + noise_set)
             m += 1
-    
+
     # save pairs with flipped surface normals
     print('\nSaving flipped normals\n')
     for i in range(part):
         # copy fragment, change the sign of the surface normals
-        fragment_neg=np.copy(fragment[i])
-        fragment_neg[:,3:6] = -fragment_neg[:,3:6]
+        fragment_neg = np.copy(fragment[i])
+        fragment_neg[:, 3:6] = -fragment_neg[:, 3:6]
 
         # generate list of used parts
         names_FN.append(f'{name}/{name}_{i}.npy')
 
         # add random displacement of points with a gaussian profile
-        noise_i=rng.normal(0,sigma,fragment[i].shape)
-        noise_neg=rng.normal(0,sigma,fragment_neg.shape)
+        noise_i = rng.normal(0, sigma, fragment[i].shape)
+        noise_neg = rng.normal(0, sigma, fragment_neg.shape)
 
         # save the two parts of the pairs as seperate .npy files
-        np.save(f'{out_folder}/flipped_normals/fragments_1/{p}.npy', fragment[i]+noise_i)
-        np.save(f'{out_folder}/flipped_normals/fragments_2/{p}.npy', fragment_neg+noise_neg)
+        np.save(f'{out_folder}/flipped_normals/fragments_1/{p}.npy', fragment[i] + noise_i)
+        np.save(f'{out_folder}/flipped_normals/fragments_2/{p}.npy', fragment_neg + noise_neg)
         p += 1
 
     # save fragments with connected surfaces 1v1
@@ -148,7 +147,6 @@ for name in names:
     #             np.save(f'{out_folder}/connected_1v1/fragments_2/{n}.npy', fragment[j]+noise_j)
     #             n += 1
 
-
 # save list of used parts to file
 # pd.DataFrame(names1_1v1).to_csv(f'{out_folder}/connected_1v1/fragments_1.csv', index=False, header=False)
 # pd.DataFrame(names2_1v1).to_csv(f'{out_folder}/connected_1v1/fragments_2.csv', index=False, header=False)
@@ -156,4 +154,4 @@ pd.DataFrame(names1_1vN).to_csv(f'{out_folder}/connected_1vN/fragments_1.csv', i
 pd.DataFrame(names2_1vN).to_csv(f'{out_folder}/connected_1vN/fragments_2.csv', index=False, header=False)
 pd.DataFrame(names_FN).to_csv(f'{out_folder}/flipped_normals/fragments.csv', index=False, header=False)
 
-print(f'Elapsed time: {time.time()-start:0.4f}')
+print(f'Elapsed time: {time.time() - start:0.4f}')
