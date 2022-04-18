a4fe2d3e3f7c2bfa2ee0d2c9b2e69b1cf143d089
MrBirrd <mathias.vogel@hotmail.ch>
2022-04-18 22:51:01 +0200

    fixed centering

--- hyperglue/hyperglue.py
+++ hyperglue/hyperglue.py
@@ -521,14 +521,16 @@ class FragmentsDataset(td.Dataset):
         kp1 = np.load(self.dataset[idx]['path_kpts_1']).astype(np.float32)
         center_0 = np.mean(kp0[:,:3], axis=0).astype(np.float32)
         center_1 = np.mean(kp1[:,:3], axis=0).astype(np.float32)
+        kp0 = np.subtract(kp0,center_0, dtype=np.float32)
+        kp1 = np.subtract(kp1,center_1, dtype=np.float32)
 
         # normalize points
-        kp0 = kp0 / max(np.max(kp0), -np.min(kp0))
-        kp1 = kp1 / max(np.max(kp1), -np.min(kp1))
+        kp0 = kp0 / np.max(np.max(kp0), np.max(-1*kp0))
+        kp1 = kp1 / np.max(np.max(kp1), np.max(-1*kp1))
 
         sample = {
-            "keypoints0": torch.from_numpy(np.subtract(kp0,center_0, dtype=np.float32)),
-            "keypoints1": torch.from_numpy(np.subtract(kp1,center_1, dtype=np.float32)),
+            "keypoints0": torch.from_numpy(kp0),
+            "keypoints1": torch.from_numpy(kp1),
             "descriptors0": torch.from_numpy(np.load(self.dataset[idx]['path_kpts_desc_0']).astype(np.float32)),
             "descriptors1": torch.from_numpy(np.load(self.dataset[idx]['path_kpts_desc_1']).astype(np.float32)),
             "gt_assignment": torch.from_numpy(gtasg),
@@ -801,7 +803,7 @@ torch.save(myGlue.state_dict(), "weights_01.pth")
 test_data = FragmentsDataset(root=root)
 
 myGlue.eval()
-test_dl = td.DataLoader(test_data, batch_size=8, shuffle=False)
+test_dl = td.DataLoader(test_data, batch_size=train_conf['batch_size'], shuffle=False)
 for it, datatest in enumerate(test_dl):
     #device = 'cuda' if torch.cuda.is_available() else 'cpu'
     #data = batch_to_device(datatest, device, non_blocking=True)
