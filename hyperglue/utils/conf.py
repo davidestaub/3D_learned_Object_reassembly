64e709465418465f4e17f9c1fec2b5ce0ea9b34b
MrBirrd <mathias.vogel@hotmail.ch>
2022-04-21 16:05:32 +0200

    Update conf.py

--- hyperglue/utils/conf.py
+++ hyperglue/utils/conf.py
@@ -7,26 +7,26 @@ model_conf = {
     'keypoint_encoder': [32, 64, 128, 256], # intermediate mlp dimensions. The first is automatically set to 3, last to 336
     'GNN_layers': ['self', 'cross'] * 9,
     'sinkhorn_iterations': 200,
-    'match_threshold': 0.5,
+    'match_threshold': 0.2,
     # 'bottleneck_dim': None,
     'loss': {
         'nll_weight': 1.,
         'nll_balancing': 0.5,
-        # 'reward_weight': 0.,
-        # 'bottleneck_l2_weight': 0.,
+        'reward_weight': 0.,
+        'bottleneck_l2_weight': 0.,
     },
 }
    
 
 train_conf = {
     'seed': 42,  # training seed
-    'epochs': 25,  # number of epochs
-    'batch_size_train': 24,  # training batch size
-    'batch_size_test': 24, #test batch size
+    'epochs': 5000,  # number of epochs
+    'batch_size_train': 32,  # training batch size
+    'batch_size_test': 32, #test batch size
     'optimizer': 'adam',  # name of optimizer in [adam, sgd, rmsprop]
     'opt_regexp': None,  # regular expression to filter parameters to optimize
     'optimizer_options': {},  # optional arguments passed to the optimizer
-    'lr': 0.0001,  # learning rate
+    'lr': 0.001,  # learning rate
     'lr_schedule': {'type': 'exp', 'start': 250e3, 'exp_div_10': 50e3},
     'eval_every_iter': 500,  # interval for evaluation on the validation set
     'log_every_iter': 100,  # interval for logging the loss to the console
