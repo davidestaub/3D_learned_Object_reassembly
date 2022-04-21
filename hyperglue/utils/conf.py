2a3954d5d7a3c7ec40b1105696e911f5cc97a163
MrBirrd <mathias.vogel@hotmail.ch>
2022-04-21 21:45:20 +0200

    mlp more like pointnet

--- hyperglue/utils/conf.py
+++ hyperglue/utils/conf.py
@@ -1,19 +1,19 @@
 model_conf = {
     'normalize_data': True, # normalizing the pointcloud
-    'use_pointnet': True, # if true, using the pointnet as encoder and ignores the mlp one
+    'use_pointnet': False, # if true, using the pointnet as encoder and ignores the mlp one
     'use_mlp': True, # using the mlp encoder, only works if pointnet is false
     'descriptor_dim': 336, # the descriptor dimension, this is what SHOT gives us
     'weights': 'weights_01',
-    'keypoint_encoder': [32, 64, 128, 256], # intermediate mlp dimensions. The first is automatically set to 3, last to 336
+    'keypoint_encoder': [64, 128, 512, 1024], # intermediate mlp dimensions. The first is automatically set to 3, last to 336
     'GNN_layers': ['self', 'cross'] * 9,
-    'sinkhorn_iterations': 50,
+    'sinkhorn_iterations': 200,
     'match_threshold': 0.2,
     # 'bottleneck_dim': None,
     'loss': {
         'nll_weight': 1.,
         'nll_balancing': 0.9,
-        'reward_weight': 0.,
-        'bottleneck_l2_weight': 0.,
+        #'reward_weight': 0.,
+        #'bottleneck_l2_weight': 0.,
     },
 }
    
@@ -21,13 +21,13 @@ model_conf = {
 train_conf = {
     'seed': 42,  # training seed
     'epochs': 5000,  # number of epochs
-    'batch_size_train': 1,  # training batch size
-    'batch_size_test': 1, #test batch size
+    'batch_size_train': 24,  # training batch size
+    'batch_size_test': 24, #test batch size
     'optimizer': 'adam',  # name of optimizer in [adam, sgd, rmsprop]
     'opt_regexp': None,  # regular expression to filter parameters to optimize
     'optimizer_options': {},  # optional arguments passed to the optimizer
-    'lr': 0.0001,  # learning rate
-    'lr_schedule': {'type': 'exp', 'start': 250e3, 'exp_div_10': 50e3},
+    'lr': 1e-3,  # learning rate
+    'lr_schedule': {'type': 'exp', 'start': 1e3, 'exp_div_10': 50e3},
     'eval_every_iter': 500,  # interval for evaluation on the validation set
     'log_every_iter': 100,  # interval for logging the loss to the console
     'keep_last_checkpoints': 5,  # keep only the last X checkpoints
