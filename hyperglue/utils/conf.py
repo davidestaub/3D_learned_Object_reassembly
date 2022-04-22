dc70f450bb0e02d7978998d44418c99e613e6494
MrBirrd <mathias.vogel@hotmail.ch>
2022-04-22 23:05:13 +0200

    Update conf.py

--- hyperglue/utils/conf.py
+++ hyperglue/utils/conf.py
@@ -5,14 +5,14 @@ model_conf = {
     'use_desc': True,
     'descriptor_dim': 336, # the descriptor dimension, this is what SHOT gives us
     'weights': 'weights_01',
-    'keypoint_encoder': [64, 128, 512, 1024], # intermediate mlp dimensions. The first is automatically set to 3, last to 336
-    'GNN_layers': ['self', 'cross'] * 7,
-    'sinkhorn_iterations': 50,
-    'match_threshold': 0.2,
+    'keypoint_encoder': [32, 64, 128, 256, 512, 1024], # intermediate mlp dimensions. The first is automatically set to 3, last to 336
+    'GNN_layers': ['self', 'cross'] * 9,
+    'sinkhorn_iterations': 40,
+    'match_threshold': 0.5,
     # 'bottleneck_dim': None,
     'loss': {
         'nll_weight': 1.,
-        'nll_balancing': 0.9,
+        'nll_balancing': 0.5,
         #'reward_weight': 0.,
         #'bottleneck_l2_weight': 0.,
     },
@@ -21,14 +21,14 @@ model_conf = {
 
 train_conf = {
     'seed': 42,  # training seed
-    'epochs': 100000,  # number of epochs
-    'batch_size_train': 16,  # training batch size
-    'batch_size_test': 16, #test batch size
+    'epochs': 5000,  # number of epochs
+    'batch_size_train': 32,  # training batch size
+    'batch_size_test': 32, #test batch size
     'optimizer': 'adam',  # name of optimizer in [adam, sgd, rmsprop]
     'opt_regexp': None,  # regular expression to filter parameters to optimize
     'optimizer_options': {},  # optional arguments passed to the optimizer
     'lr': 0.001,  # learning rate
-    'lr_schedule': {'type': 'exp', 'start': 1e3, 'exp_div_10': 50e3},
+    'lr_schedule': {'type': 'exp', 'start': 250e3, 'exp_div_10': 50e3},
     'eval_every_iter': 500,  # interval for evaluation on the validation set
     'log_every_iter': 100,  # interval for logging the loss to the console
     'keep_last_checkpoints': 5,  # keep only the last X checkpoints
