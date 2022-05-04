81f67228d88fa40bfb565d7a0ebbd8c222a2561f
MrBirrd <mathias.vogel@hotmail.ch>
2022-05-04 16:18:34 +0200

    Update visualize_keypoints.py

--- object_fracturing/visualize_keypoints.py
+++ object_fracturing/visualize_keypoints.py
@@ -1,12 +1,11 @@
-from glob import glob
 import os
-from turtle import window_height, window_width
 import open3d as o3d
 import numpy as np
 
 # chose a data folder
+folder = "data"
 here = os.path.dirname(os.path.abspath(__file__))
-data_list = os.listdir(os.path.join(here, 'data'))
+data_list = os.listdir(os.path.join(here, folder))
 
 
 print("id  name")
@@ -14,7 +13,7 @@ for idx, val in enumerate(data_list):
     print(idx, " ", val)
 idx = int(input("Enter the index of the subfolder in data where the shards are located:\n"))
 subfolder = data_list[idx]
-ROOT = os.path.join(here, 'data', subfolder)
+ROOT = os.path.join(here, folder, subfolder)
 
 # get the other folders
 CLEANED = os.path.join(ROOT, 'cleaned')
@@ -33,7 +32,7 @@ file = data_list[idx]
 idx = int(file.split('cleaned.')[1].split('.')[0])
 print(file, idx)
 # extract the corresponding filename
-kpts_file_sticky = f'keypoints_sticky.{idx}.npy'
+kpts_file_sticky = f'keypoints_hybrid.{idx}.npy'
 kpts_sticky = np.load(os.path.join(KPTS_IN, kpts_file_sticky))[:,:3]
 pcd_kpts = o3d.geometry.PointCloud()
 pcd_kpts.points= o3d.utility.Vector3dVector(kpts_sticky)
