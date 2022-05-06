22513be0400d507655029259f4085d2393a16e4f
MrBirrd <mathias.vogel@hotmail.ch>
2022-05-06 12:31:29 +0200

    Update sweep.py

--- hyperglue/sweep.py
+++ hyperglue/sweep.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     for run in range(5):
         subprocess.check_call(
             [
-                "bsub -n 4 -R \"rusage[ngpus_excl_p=1, mem=5000]\" -R \"select[gpu_mtotal0>=10240]\" "
+                "bsub",
+                "-n 4 -R \"rusage[ngpus_excl_p=1, mem=5000]\" -R \"select[gpu_mtotal0>=10240]\" "
                 "wandb agent matvogel/3D_learned_Object_reassembly-hyperglue/fj91yu0g",
             ]
         )
