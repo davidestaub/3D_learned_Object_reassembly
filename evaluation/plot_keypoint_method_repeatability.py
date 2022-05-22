4f36fa02c95b39efca0b89d7f8c8f62aa4c42963
Julia Bazinska <julia@bazinski.com>
2022-05-22 13:55:11 +0200

    Improve plotting

--- evaluation/plot_keypoint_method_repeatability.py
+++ evaluation/plot_keypoint_method_repeatability.py
@@ -3,6 +3,34 @@ import numpy as np
 import pandas as pd
 from matplotlib.ticker import PercentFormatter
 
+
+def plot_dists(dists, df, method, ax):
+    data = dists[df['method'] == method].reshape(-1)
+    ax.hist(data, bins=np.linspace(0, np.nanmax(dists), 100), weights=np.ones_like(data) / len(data))
+    ax.yaxis.set_major_formatter(PercentFormatter(1))
+    ax.set_xlim((0, np.nanmax(dists)))
+    # ax.set_ylim((0, 0.07))
+    ax.set_xlabel('Distance')
+    ax.set_ylabel('# points')
+    ax.set_title(f'Method: {method}, n={len(data)}')
+
+
+def plot_cumulative_dists(dists, df, method, ax, title=''):
+    data = dists[df['method'] == method].reshape(-1)
+    ax.hist(data,
+            bins=np.linspace(0, np.nanmax(dists), 500),
+            density=True,
+            cumulative=True,
+            histtype='step',
+            label=method)
+    ax.yaxis.set_major_formatter(PercentFormatter(1))
+    ax.set_xlim((0, 1.0))
+    # ax.set_ylim((0, 0.07))
+    ax.set_xlabel('Distance')
+    ax.set_ylabel('# points')
+    ax.set_title(title)
+
+
 def plot_surface():
     csv_path = 'keypoint_surface_repeatability.csv'
     df = pd.read_csv(csv_path)
@@ -10,39 +38,14 @@ def plot_surface():
     df = df.loc[:, :'fragment_2']
     methods = df['method'].unique()
 
-    def plot_dists(method, ax):
-        data = dists[df['method'] == method].reshape(-1)
-        ax.hist(data, bins=np.linspace(0, np.nanmax(dists), 100), weights=np.ones(len(data)) / len(data))
-        ax.yaxis.set_major_formatter(PercentFormatter(1))
-        ax.set_xlim((0, np.nanmax(dists)))
-        ax.set_ylim((0, 0.07))
-        ax.set_xlabel('Distance')
-        ax.set_ylabel('# points')
-        ax.set_title(f'Method: {method}, n={len(data)}')
-
     fig = plt.figure(figsize=(6, 12))
     axes = fig.subplots(len(methods), 1, sharex=True)
-    [plot_dists(method, ax) for method, ax in zip(methods, axes)]
+    [plot_dists(dists, df, method, ax) for method, ax in zip(methods, axes)]
     plt.savefig('keypoint_surface_repeatability.png')
     fig.show()
 
-    def plot_cumulative_dists(method, ax):
-        data = dists[df['method'] == method].reshape(-1)
-        ax.hist(data,
-                bins=np.linspace(0, np.nanmax(dists), 500),
-                weights=np.ones(len(data)) / len(data),
-                cumulative=True,
-                histtype='step',
-                label=method)
-        ax.yaxis.set_major_formatter(PercentFormatter(1))
-        ax.set_xlim((0, 1.0))
-        # ax.set_ylim((0, 0.07))
-        ax.set_xlabel('Distance')
-        ax.set_ylabel('# points')
-        ax.set_title(f'Cumulative nearest matched surface keypoint distance distribution')
-
     fig = plt.figure(figsize=(6, 4))
-    [plot_cumulative_dists(method, fig.gca()) for method in methods]
+    [plot_cumulative_dists(dists, df, method, fig.gca()) for method in methods]
     fig.gca().legend(loc='lower right')
     plt.grid(visible=True)
     plt.savefig('keypoint_surface_repeatability_cumulative.png')
@@ -65,39 +68,14 @@ def plot():
     df = df.loc[:, :'method']
     methods = df['method'].unique()
 
-    def plot_dists(method, ax):
-        data = dists[df['method'] == method].reshape(-1)
-        ax.hist(data, bins=np.linspace(0, np.nanmax(dists), 100), weights=np.ones(len(data)) / len(data))
-        ax.yaxis.set_major_formatter(PercentFormatter(1))
-        ax.set_xlim((0, np.nanmax(dists)))
-        ax.set_ylim((0, 0.07))
-        ax.set_xlabel('Distance')
-        ax.set_ylabel('# points')
-        ax.set_title(f'Method: {method}, n={len(data)}')
-
     fig = plt.figure(figsize=(6, 12))
     axes = fig.subplots(len(methods), 1, sharex=True)
-    [plot_dists(method, ax) for method, ax in zip(methods, axes)]
+    [plot_dists(dists, df, method, ax) for method, ax in zip(methods, axes)]
     plt.savefig('keypoint_repeatability.png')
     fig.show()
 
-    def plot_cumulative_dists(method, ax):
-        data = dists[df['method'] == method].reshape(-1)
-        ax.hist(data,
-                bins=np.linspace(0, np.nanmax(dists), 500),
-                weights=np.ones(len(data)) / len(data),
-                cumulative=True,
-                histtype='step',
-                label=method)
-        ax.yaxis.set_major_formatter(PercentFormatter(1))
-        ax.set_xlim((0, 1.0))
-        # ax.set_ylim((0, 0.07))
-        ax.set_xlabel('Distance')
-        ax.set_ylabel('# points')
-        ax.set_title(f'Cumulative nearest keypoint distance distribution')
-
     fig = plt.figure(figsize=(6, 4))
-    [plot_cumulative_dists(method, fig.gca()) for method in methods]
+    [plot_cumulative_dists(dists, df, method, fig.gca()) for method in methods]
     fig.gca().legend(loc='lower right')
     plt.grid(visible=True)
     plt.savefig('keypoint_repeatability_cumulative.png')
